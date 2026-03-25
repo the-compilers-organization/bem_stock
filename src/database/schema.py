8da7fc3883e0ec11113c2be32ea2053f1e92aa98
Francis Lauriano <130801505+FrancisLauriano@users.noreply.github.com>
2026-03-25 15:56:37 -0300

    Update schema.py

--- src/database/schema.py
+++ src/database/schema.py
@@ -1,1 +1,54 @@
+from database.connection import get_connection
 
+
+def create_tables():
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS usuarios (
+            id_usuario INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome TEXT NOT NULL,
+            email TEXT NOT NULL UNIQUE,
+            senha TEXT NOT NULL,
+            perfil TEXT NOT NULL CHECK (perfil IN ('admin', 'estoque'))
+        )
+    """)
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS produtos (
+            id_produto INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome TEXT NOT NULL,
+            descricao TEXT,
+            categoria TEXT NOT NULL CHECK (
+                categoria IN (
+                    'Alimentos',
+                    'Produtos de Higiene',
+                    'Produtos de Limpeza'
+                )
+            ),
+            quantidade_atual INTEGER NOT NULL DEFAULT 0,
+            unidade_medida TEXT NOT NULL,
+            estoque_minimo INTEGER NOT NULL,
+            data_validade TEXT NOT NULL
+        )
+    """)
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS movimentacoes (
+            id_movimentacao INTEGER PRIMARY KEY AUTOINCREMENT,
+            tipo_movimentacao TEXT NOT NULL CHECK (
+                tipo_movimentacao IN ('entrada', 'saida')
+            ),
+            quantidade INTEGER NOT NULL,
+            data_movimentacao TEXT NOT NULL,
+            observacao TEXT,
+            id_produto INTEGER NOT NULL,
+            id_usuario INTEGER NOT NULL,
+            FOREIGN KEY (id_produto) REFERENCES produtos(id_produto),
+            FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario)
+        )
+    """)
+
+    conn.commit()
+    conn.close()
